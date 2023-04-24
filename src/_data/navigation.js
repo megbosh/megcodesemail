e6f4ba593039050bf2a406e95600514b51785296
megbosh <59982423+megbosh@users.noreply.github.com>
2023-04-24 14:35:32 -0400

    Update navigation.js

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -7,16 +7,10 @@ module.exports = {
     {
       text: 'Blog',
       url: '/blog/'
-    }
-  ],
-  bottom: [
-    {
-      text: 'Imprint',
-      url: '/imprint/'
     },
     {
-      text: 'Privacy',
-      url: '/privacy/'
+      text: 'Newsletter',
+      url: 'https://itdependsnewsletter.beehiiv.com/'
     }
   ]
 };

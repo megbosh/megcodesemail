9191d54dd3fa213759277fa22cd599f16493fbc8
megbosh <59982423+megbosh@users.noreply.github.com>
2023-04-24 14:41:29 -0400

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,30 +1,29 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
-  siteName: 'Eleventy Excellent',
+  siteName: 'MegCodesEmail',
   siteDescription:
-    'Eleventy starter based on the workflow suggested by buildexcellentwebsit.es.',
+    'Portfolio website from Megan Boshuyzen, award-winning senior email developer',
   siteType: 'Person', // schema
   locale: 'en_EN',
   lang: 'en',
   skipContent: 'Skip to content',
-  author: 'Lene Saile', // i.e. Lene Saile - author's name. Must be set.
+  author: '', // i.e. Lene Saile - author's name. Must be set.
   authorEmail: '', // i.e. hola@lenesaile.com - email of the author
   authorWebsite: '', // i.e. https.://www.lenesaile.com - the personal site of the author
   themeColor: '#58153E', //  Manifest: defines the default theme color for the application
   themeBgColor: '#ffffff', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
-    opengraph_default: '/assets/images/opengraph-default.jpg', // fallback/default meta image
+    opengraph_default: '/assets/images/logo.svg', // fallback/default meta image
     opengraph_default_alt:
-      'Visible content: Eleventy starter based on workflow for Cube CSS, Every Layout, Design Tokens and Tailwind for uitility, based on the concepts explained in buildexcellentwebsit.es ', // alt text for default meta image
+      'Visible content: MegCodesEmail logo ', // alt text for default meta image
     twitterSite: '',
-    twitterCreator: '@megcodesemail',
-    mastodonProfile: ''
+    twitterCreator: '@megcodesemail'
   },
   blog: {
     // this is for the rss feed
     name: 'MegCodesEmail',
     description:
-      'Tell the word what you are writing about in your blog! It will show up on feed readers.'
+      'Recent works from Megan Boshuyzen'
   },
   pagination: {
     itemsPerPage: 20
